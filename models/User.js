92140c23f236d004fb05f928e9d58693b631a8a8
islam osama <153915275+islam-osama2024@users.noreply.github.com>
2026-07-17 22:57:59 +0300

    Add password reset fields to User model
    Added fields for password reset token and expiration.

--- models/User.js
+++ models/User.js
@@ -23,6 +23,12 @@ const userSchema = new mongoose.Schema(
       enum: ['user', 'admin'],
       default: 'user',
     },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date,
+    },
   },
   { timestamps: true }
 );
